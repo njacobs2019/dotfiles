8aa509a90108e5e9dc75f49af5c4e549c9b1409f
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2023-08-24 11:55:49 -0400

    feat: Install programs

---
+++ install/gnome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo dnf install gnome-tweaks  # rpm from Fedora repo
+flatpak install org.gnome.Extensions # from Fedora repo
+
---
+++ install/programs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+dnf install google-chrome-stable -y
+flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub com.slack.Slack -y
+flatpak install com.spotify.Client -y
+
+# Setup VS-Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install code -y
