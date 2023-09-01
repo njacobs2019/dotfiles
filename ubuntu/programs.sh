582e99e6c315b787c43594cc4696530fe5a48852
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2023-08-31 22:08:14 -0400

    Updated Ubuntu Programs

--- ubuntu/programs.sh
+++ ubuntu/programs.sh
@@ -10,6 +10,10 @@ sudo apt install dconf-editor gnome-tweaks gnome-shell-extension-manager -y
 sudo apt install solaar spotify-client -y
 
 # Productivity tools
-sudo apt install google-chrome-stable code -y
+sudo apt install google-chrome-stable -y
 flatpak install flathub net.cozic.joplin_desktop -y
+
+# Programming tools
+sudo apt install code -y
+sudo apt install git-lfs -y
 
