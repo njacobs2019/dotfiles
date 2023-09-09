87152b628aabd7a413e9902332872ffa2051af1a
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2023-09-09 17:03:00 -0400

    Added terminal transparency

--- gnome_settings.sh
+++ gnome_settings.sh
@@ -3,7 +3,7 @@
 # gnome settings
 gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar "minimize"
 gsettings set org.gnome.desktop.wm.preferences focus-mode "click"
-gsettings set org.gnome.desktop.wm.preferences button-layout ":close"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,close"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
 gsettings set org.gnome.desktop.background picture-uri "/home/nick/dotfiles/desktop_images/Optical_Fibers_in_Dark_by_Elena_Stravoravdi.jpeg"
 gsettings set org.gnome.shell.keybindings show-screenshot-ui "['<Shift><Super>s']"
@@ -13,6 +13,8 @@ gsettings set org.gnome.shell.extensions.ding start-corner "top-left"
 # Gnome terminal settings
 PROFILE_ID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d "'")
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/ background-transparency-percent 20
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/ use-theme-transparency false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${PROFILE_ID}/ use-transparent-background true
 gsettings set org.gnome.Terminal.Legacy.Settings new-terminal-mode 'tab'
 
 # dash to dock settings
@@ -31,4 +33,3 @@ gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 
 # nautilus settings
 gsettings set org.gtk.Settings.FileChooser show-hidden true
-
