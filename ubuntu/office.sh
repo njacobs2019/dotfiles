d3f7eaae669a4f37615a556e48b4e433973d1521
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2023-09-24 16:13:51 -0400

    Office settings

---
+++ ubuntu/office.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Uninstall LibreOffice
+sudo apt remove libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-draw libreoffice-base libreoffice-math -y
+
+# Install LibreOffice Flatpak
+flatpak install flathub org.libreoffice.LibreOffice -y
+
+# This installs Microsoft TrueType core fonts
+# Press tab to select an option at the configuration page, then enter to select it
+# sudo apt install ttf-mscorefonts-installer -y
+
+# Install Zotero
+flatpak install flathub org.zotero.Zotero -y
