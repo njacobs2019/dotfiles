c03947bc7ebb42343ab2b878ab8356140092930d
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2023-09-26 13:34:33 -0400

    Install microsoft fonts

--- ubuntu/office.sh
+++ ubuntu/office.sh
@@ -8,7 +8,7 @@ flatpak install flathub org.libreoffice.LibreOffice -y
 
 # This installs Microsoft TrueType core fonts
 # Press tab to select an option at the configuration page, then enter to select it
-# sudo apt install ttf-mscorefonts-installer -y
+sudo apt install ttf-mscorefonts-installer -y
 
 # Install Zotero
 flatpak install flathub org.zotero.Zotero -y
