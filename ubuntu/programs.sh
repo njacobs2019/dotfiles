f80cd92866ed628b6ebc833c79b4966e4e060845
Nicholas Jacobs <59352054+njacobs2019@users.noreply.github.com>
2023-09-26 13:35:29 -0400

    Moved nextcloud and xournalpp (flatpak)

--- ubuntu/programs.sh
+++ ubuntu/programs.sh
@@ -8,12 +8,13 @@ sudo apt install dconf-editor gnome-tweaks gnome-shell-extension-manager -y
 
 # Utilities
 sudo apt install solaar spotify-client -y
+sudo apt install nautilus-nextcloud -y
 
 # Productivity tools
 sudo apt install google-chrome-stable -y
 flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub com.github.xournalpp.xournalpp -y
 
 # Programming tools
 sudo apt install code -y
 sudo apt install git-lfs -y
-
--- ubuntu/programs_personal.sh
+++ ubuntu/programs_personal.sh
@@ -3,10 +3,8 @@
 # This installs programs designated for personal devices
 
 flatpak install com.discordapp.Discord -y
-sudo apt install xournalpp -y
 
 # cloudflared @ /usr/local/bin/cloudflared
 sudo apt install cloudflared -y
 
 sudo apt install cryptomator -y
-sudo apt install nautilus-nextcloud -y
